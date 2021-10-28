3aa19aac9fc813867fe266590f9f0ee634646f80
cyy <cyyever@outlook.com>
2021-10-28 21:40:51 +0800

    fix code

--- python_binding/stochastic_quantization.hpp
+++ python_binding/stochastic_quantization.hpp
@@ -8,7 +8,7 @@
 #include <pybind11/pybind11.h>
 #include <torch/extension.h>
 
-#include "torch/stochastic_quantization.hpp"
+#include "stochastic_quantization.hpp"
 namespace py = pybind11;
 inline void define_torch_quantization_extension(py::module_ &m) {
   auto sub_m = m.def_submodule("torch", "Contains pytorch extension");
--- python_binding/synced_tensor_dict.hpp
+++ python_binding/synced_tensor_dict.hpp
@@ -8,8 +8,8 @@
 #include <pybind11/pybind11.h>
 #include <torch/extension.h>
 
-#include "torch/synced_sparse_tensor_dict.hpp"
-#include "torch/synced_tensor_dict.hpp"
+#include "synced_sparse_tensor_dict.hpp"
+#include "synced_tensor_dict.hpp"
 namespace py = pybind11;
 inline void define_torch_data_structure_extension(py::module_ &m) {
   using synced_tensor_dict = cyy::naive_lib::pytorch::synced_tensor_dict;
