a06359f403d06cce27906aa83fac2790512c4c94
cyy <cyyever@outlook.com>
2022-01-13 16:20:24 +0800

    add log

--- src/stochastic_quantization_kernel.cpp
+++ src/stochastic_quantization_kernel.cpp
@@ -1,4 +1,5 @@
 
+#include <cyy/naive_lib/log/log.hpp>
 #include "stochastic_quantization.hpp"
 #include <ATen/TensorIterator.h>
 #include <ATen/native/cpu/Loops.h>
@@ -13,6 +14,7 @@ void stochastic_quantization_cpu(at::Tensor &slot_ret, const at::Tensor &src,
                   .add_input(at::get_tensor_base(src))
                   .build();
 
+  LOG_WARN("use cpu for quantization");
   at::native::cpu_kernel(
       iter, [quantization_level](const float src_val) -> float {
         auto slot = static_cast<uint64_t>(src_val * quantization_level);
