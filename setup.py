ecff29603180f06624f5886a4b892affec132b00
cyy <cyyever@outlook.com>
2022-01-16 17:11:55 +0800

    add code

--- setup.py
+++ setup.py
@@ -22,10 +22,15 @@ class CMakeBuild(build_ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
 
         os.makedirs(extdir, exist_ok=True)
+        os.makedirs(self.build_temp, exist_ok=True)
 
         cmake_build_dir = os.getenv("cmake_build_dir")
         for f in Path(cmake_build_dir).rglob("*"):
-            if str(f).endswith(".so") or str(f).endswith(".dll"):
+            if (
+                str(f).endswith(".so")
+                or str(f).endswith(".dll")
+                or str(f).endswith(".lib")
+            ):
                 shutil.copy(f, extdir)
             if str(f).endswith(".lib"):
                 shutil.copy(f, self.build_temp)
