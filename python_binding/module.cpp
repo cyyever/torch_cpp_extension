3ee2a65bd907e036c2ee960d6df58c4a340a711b
cyy <cyyever@outlook.com>
2022-01-29 14:32:19 +0800

    remove quantization

--- python_binding/module.cpp
+++ python_binding/module.cpp
@@ -1,9 +1,7 @@
-#include "stochastic_quantization.hpp"
 #include "synced_tensor_dict.hpp"
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
 
 PYBIND11_MODULE(cyy_torch_cpp_extension, m) {
   define_torch_data_structure_extension(m);
-  define_torch_quantization_extension(m);
 }
