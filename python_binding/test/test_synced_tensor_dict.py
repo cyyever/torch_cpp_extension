9f70287e1747844586fbadc8397fc6ac73390fb3
cyy <cyyever@outlook.com>
2022-08-14 16:59:10 +0800

    fix code

--- python_binding/test/test_synced_tensor_dict.py
+++ python_binding/test/test_synced_tensor_dict.py
@@ -1,20 +1,20 @@
-import pathlib
+import tempfile
 
 import cyy_torch_cpp_extension
 import torch
 
 
 def test_synced_tensor_dict():
-    tensor_dict = cyy_torch_cpp_extension.data_structure.SyncedTensorDict("")
-    tensor_dict.set_in_memory_number(10)
-    tensor_dict.set_storage_dir(pathlib.Path("tensor_dict_dir"))
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tensor_dict = cyy_torch_cpp_extension.data_structure.SyncedTensorDict(tmpdir)
+        tensor_dict.set_in_memory_number(10)
 
-    # tensor_dict.set_permanent_storage()
-    for i in range(100):
-        tensor_dict[str(i)] = torch.Tensor([i])
+        # tensor_dict.set_permanent_storage()
+        for i in range(100):
+            tensor_dict[str(i)] = torch.Tensor([i])
 
-    tensor_dict.prefetch([str(i) for i in range(100)])
-    assert str(0) in tensor_dict
-    for i in tensor_dict.keys():
-        assert tensor_dict[i] == torch.Tensor([int(i)])
-    tensor_dict.release()
+        tensor_dict.prefetch([str(i) for i in range(100)])
+        assert str(0) in tensor_dict
+        for i in tensor_dict.keys():
+            assert tensor_dict[i] == torch.Tensor([int(i)])
+        tensor_dict.release()
