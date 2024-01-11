b35b338ef4ca4ef62829d78e5ce67a8eadaad602
cyy <cyyever@outlook.com>
2024-01-11 14:17:40 +0800

    Use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,47 +1,72 @@
-# -*- coding: utf-8 -*-
+# Available at setup time due to pyproject.toml
 import os
-import shutil
+import subprocess
+import sys
 from pathlib import Path
 
+import ninja
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
+
+__version__ = "0.2"
+
 
 # A CMakeExtension needs a sourcedir instead of a file list.
 # The name must be the _single_ output extension from the CMake build.
 # If you need multiple extensions, see scikit-build.
-
-
 class CMakeExtension(Extension):
-    def __init__(self, name):
-        Extension.__init__(self, name, sources=[])
+    def __init__(self, name: str, sourcedir: str = "") -> None:
+        super().__init__(name, sources=[])
+        self.sourcedir = os.fspath(Path(sourcedir).resolve())
 
 
 class CMakeBuild(build_ext):
-    def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        os.makedirs(extdir, exist_ok=True)
-        cmake_build_dir = os.getenv("cmake_build_dir")
-        for f in Path(cmake_build_dir).rglob("*"):
-            if (
-                str(f).endswith(".so")
-                or str(f).endswith(".dll")
-                or str(f).endswith(".lib")
-                or str(f).endswith(".pyd")
-            ):
-                shutil.copy(f, extdir)
+    def build_extension(self, ext: CMakeExtension) -> None:
+        # Must be in this form due to bug in .resolve() only fixed in Python 3.10+
+        ext_fullpath = Path.cwd() / self.get_ext_fullpath(ext.name)
+        extdir = ext_fullpath.parent.resolve()
+
+        # Using this requires trailing slash for auto-detection & inclusion of
+        # auxiliary "native" libs
+
+        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
+        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
+        # from Python.
+        cmake_args = [
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}{os.sep}",
+            f"-DPYTHON_EXECUTABLE={sys.executable}",
+        ]
+        build_args = []
+        # Adding CMake arguments set as environment variable
+        # (needed e.g. to build for ARM OSx on conda-forge)
+        if "CMAKE_ARGS" in os.environ:
+            cmake_args += [item for item in os.environ["CMAKE_ARGS"].split(" ") if item]
+
+        ninja_executable_path = Path(ninja.BIN_DIR) / "ninja"
+        cmake_args += [
+            "-GNinja",
+            f"-DCMAKE_MAKE_PROGRAM:FILEPATH={ninja_executable_path}",
+        ]
+
+        build_temp = Path(self.build_temp) / ext.name
+        if not build_temp.exists():
+            build_temp.mkdir(parents=True)
+
+        subprocess.run(
+            ["cmake", ext.sourcedir, *cmake_args], cwd=build_temp, check=True
+        )
+        subprocess.run(
+            ["cmake", "--build", ".", *build_args], cwd=build_temp, check=True
+        )
 
 
-# The information here can also be placed in setup.cfg - better separation of
-# logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="cyy_torch_cpp_extension",
-    version="0.1",
-    author="cyy",
-    author_email="cyyever@outlook.com",
-    description="Python binding for this C++ lib",
-    url="https://github.com/cyyever/torch_cpp_extension",
-    ext_modules=[CMakeExtension("cmake_example")],
+    version=__version__,
+    # Currently, build_ext only provides an optional "highest supported C++
+    # level" feature, but in the future it may provide more features.
+    ext_modules=[CMakeExtension("cyy_torch_cpp_extension")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
-    python_requires=">=3.6",
+    python_requires=">=3.11",
 )
